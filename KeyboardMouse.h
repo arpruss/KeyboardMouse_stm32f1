0a84b75262e46ed2b0d0919ddb56e8a00cd5b74a
arpruss <arpruss@gmail.com>
2017-12-03 14:18:32 -0600

    initial import

---
+++ KeyboardMouse.h
@@ -0,0 +1,6 @@
+#ifndef _KEYBOARDMOUSEJOYSTICK_H
+#define _KEYBOARDMOUSEJOYSTICK_H
+
+#include "usb_hid_device.h"
+
+#endif
---
+++ midihid.h
@@ -0,0 +1,7 @@
+#ifndef _MIDIHID_H
+#define _MIDIHID_H
+
+#define USB_HID_KM
+#define USB_HARDWARE
+
+#endif
